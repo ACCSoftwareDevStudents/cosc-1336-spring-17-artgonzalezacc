9076c324b441cd2c0c825119724d2495c338a468
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2017-04-27 20:47:37 -0500

    add navigation, search, and file save

--- gui/frames/aboutframe.py
+++ gui/frames/aboutframe.py
@@ -7,4 +7,10 @@ class AboutFrame(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
 
-        Label(self, text="About Frame").grid(row=0, column=0, sticky="w")
+        label_text = "We were inspired by Microsoft's Contoso University example \n" + \
+        "https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/ \n " + \
+        "getting-started-with-ef-using-mvc/creating-an-entity-framework-data-model-for-an-asp-net-mvc-application \n" + \
+        "THIS IS NOT OUR ORIGINAL IDEA"
+                     
+
+        Label(self, text=label_text).grid(row=0, column=0, sticky="w")
